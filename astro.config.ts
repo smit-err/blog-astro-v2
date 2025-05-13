297f286a7357297d54657f1376e848bf25291df0
smit <smitmistry19@gmail.com>
2025-05-13 10:05:07 +0530

    trailing slash always

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   markdown: { shikiConfig: { theme: "houston" } },
+  trailingSlash: "always",
   build: {
     inlineStylesheets: "never",
   },
