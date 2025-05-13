c3dc244f06611637ec13dd29ad524970e9cf5bec
smit <smitmistry19@gmail.com>
2025-05-13 10:07:53 +0530

    seperated portfolio data

---
+++ src/data/portfolio.ts
@@ -0,0 +1,72 @@
+import Placeholder from "../assets/placeholder.webp";
+import Company from "../assets/project-logos/company.svg";
+
+export const FEATURED_WORK = [
+  {
+    logo: Company,
+    title: "NexaMotion Group",
+    short_subheading: "Simplifying complex vehicle repair",
+    year: "2025",
+    category: "design & development",
+    image: Placeholder,
+  },
+  {
+    logo: Company,
+    title: "Modern Font Stacks",
+    short_subheading: "Font stack CSS for modern OSs",
+    year: "2024",
+    category: "side project",
+    image: Placeholder,
+  },
+  {
+    logo: Company,
+    title: "Transend",
+    short_subheading: "Auto parts e-commerce platform",
+    year: "2024",
+    category: "design & development",
+    image: Placeholder,
+  },
+  {
+    logo: Company,
+    title: "The Flaming Candle",
+    short_subheading: "Artisan candle making supplies",
+    year: "2024",
+    category: "design & development",
+    image: Placeholder,
+  },
+];
+
+export const PORTFOLIO = [
+  {
+    logo: Company,
+    title: "NexaMotion Group",
+    short_subheading: "Simplifying complex vehicle repair",
+    year: "2025",
+    category: "design & development",
+    image: Placeholder,
+  },
+  {
+    logo: Company,
+    title: "Modern Font Stacks",
+    short_subheading: "Font stack CSS for modern OSs",
+    year: "2024",
+    category: "side project",
+    image: Placeholder,
+  },
+  {
+    logo: Company,
+    title: "Transend",
+    short_subheading: "Auto parts e-commerce platform",
+    year: "2024",
+    category: "design & development",
+    image: Placeholder,
+  },
+  {
+    logo: Company,
+    title: "The Flaming Candle",
+    short_subheading: "Artisan candle making supplies",
+    year: "2024",
+    category: "design & development",
+    image: Placeholder,
+  },
+];
