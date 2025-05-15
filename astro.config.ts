51409f0237059bd00b065ff83c88293db0a6ff6a
smit <smitmistry19@gmail.com>
2025-05-15 10:59:50 +0530

    added prefetch on load

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   markdown: { shikiConfig: { theme: "houston" } },
+  prefetch: {
+    defaultStrategy: "load",
+  },
   devToolbar: { enabled: false },
 });
