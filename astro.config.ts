72c7693405c2a467b6a6b59ef14a187dda5de8dd
smit <smitmistry19@gmail.com>
2025-05-15 10:46:53 +0530

    removed inline style threshold

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   markdown: { shikiConfig: { theme: "houston" } },
-  trailingSlash: "always",
-  vite: {
-    build: {
-      assetsInlineLimit: 1024,
-    },
-  },
   devToolbar: { enabled: false },
 });
